ded38392cab18e990e9fee2ab45f1e1e4ef707a2
shuvro-das <shuvrodas1999@gmail.com>
2021-06-14 22:09:12 +0530

    third commit shuvro

--- client/src/screen/FindSessionScreen.js
+++ client/src/screen/FindSessionScreen.js
@@ -3,7 +3,7 @@ import React from "react";
 function FindSessionScreen() {
   return (
     <div>
-      <div>Find Session Screen</div>
+      <div>Find Session Screen. come join here</div>
     </div>
   );
 }
